730c5300bac482a6a2d16c63062eae457314f3bf
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-11-19 13:46:16 -0800

    remove etras

--- hello.py
+++ hello.py
@@ -4,8 +4,6 @@ from openai_helper import OpenAIHelper
 MODEL = "gpt-4-vision-preview"
 MAX_TOKENS = 300
 
-openai_helper = OpenAIHelper(model=MODEL, max_tokens=MAX_TOKENS)
-
 def payload(base64_img):
     return [
         {
@@ -25,11 +23,9 @@ def payload(base64_img):
 
 #                 {"type": "text", "text": "Analyze the provided image and locate the iphone. Return the coordinates of the iphone in a structured format. Provide the top-left corner, bottom-right corner coordinates and orientation in the format: 'Top-left: (x1, y1), Bottom-right: (x2, y2), Orientation: value'. Also, describe its orientation as either 'horizontal' or 'vertical'."},
 
+# Initialize openai
+openai_helper = OpenAIHelper(model=MODEL, max_tokens=MAX_TOKENS)
 
-# Create an instance of OpenAIHelper
-openai_helper = OpenAIHelper(model="gpt-4-vision-preview", max_tokens=300)
-
-# Use the methods of OpenAIHelper
 img_path = "frames/frame.jpg"
 encoded_image = openai_helper.encode_img(img_path)
 req_payload = payload(encoded_image)
