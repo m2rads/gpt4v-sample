928a145dbe7cf5d260f9e5b34d45d506e43525c8
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-11-19 13:45:07 -0800

    fixing bugs

--- hello.py
+++ hello.py
@@ -1,34 +1,20 @@
 import os 
-from openai import OpenAI
-import base64
-import json
-import time
-import errno
-
-client = OpenAI()
+from openai_helper import OpenAIHelper
 
 MODEL = "gpt-4-vision-preview"
 MAX_TOKENS = 300
 
-
-def encode_img(img_path):
-    while True:
-        try:
-            with open(img_path, 'rb') as img_file:
-                return base64.b64encode(img_file.read()).decode("utf-8")
-        except IOError as e:
-            if e.errno != errno.EACCES:
-                # Not a "file in use" error, re-raise
-                raise
-            # File is being written to, wait a bit and retry
-            time.sleep(0.1)
+openai_helper = OpenAIHelper(model=MODEL, max_tokens=MAX_TOKENS)
 
 def payload(base64_img):
     return [
         {
             "role": "user",
             "content": [
-                {"type": "text", "text": "This is a math lecture. Identify the math expressions and return what you see on the board as LaTex code only."},
+                {
+                    "type": "text", 
+                    "text": "Analyze the provided image and locate the iphone. Return the coordinates of the iphone in a structured format. Provide the top-left corner, bottom-right corner coordinates and orientation in the format: 'Top-left: (x1, y1), Bottom-right: (x2, y2), Orientation: value'. Also, describe its orientation as either 'horizontal' or 'vertical'."
+                },
                 {
                     "type": "image_url",
                     "image_url": f"data:image/jpeg;base64,{base64_img}"
@@ -37,36 +23,42 @@ def payload(base64_img):
         }
     ]
 
-
-def hit_openai(base64_img): 
-    response = client.chat.completions.create(
-        model=MODEL,
-        messages=payload(base64_img),
-        max_tokens=MAX_TOKENS
-    )    
-    response_text = response.choices[0].message.content
-    return response
+#                 {"type": "text", "text": "Analyze the provided image and locate the iphone. Return the coordinates of the iphone in a structured format. Provide the top-left corner, bottom-right corner coordinates and orientation in the format: 'Top-left: (x1, y1), Bottom-right: (x2, y2), Orientation: value'. Also, describe its orientation as either 'horizontal' or 'vertical'."},
 
 
-def main(): 
+# Create an instance of OpenAIHelper
+openai_helper = OpenAIHelper(model="gpt-4-vision-preview", max_tokens=300)
 
-    while True: 
-        # path to your image
-        img_path = os.path.join(os.getcwd(), "./frames/frame.jpg")
+# Use the methods of OpenAIHelper
+img_path = "frames/frame.jpg"
+encoded_image = openai_helper.encode_img(img_path)
+req_payload = payload(encoded_image)
+response = openai_helper.hit_openai(req_payload)
 
-        # getting the base64 encoding 
-        base64_img = encode_img(img_path)
-
-        # analyze posture
-        print("openai is calculating :)")
-        latex = hit_openai(base64_img)
-
-        print("openai returned")
-        print(latex)
-
-        # wait for 5 sec 
-        time.sleep(5)
+print(response)
 
 
-if __name__ == "__main__":
-    main()
+
+
+# def main(): 
+
+#     while True: 
+#         # path to your image
+#         img_path = os.path.join(os.getcwd(), "./frames/frame.jpg")
+
+#         # getting the base64 encoding 
+#         base64_img = encode_img(img_path)
+
+#         # analyze posture
+#         print("openai is calculating :)")
+#         latex = hit_openai(base64_img)
+
+#         print("openai returned")
+#         print(latex)
+
+#         # wait for 5 sec 
+#         time.sleep(5)
+
+
+# if __name__ == "__main__":
+#     main()
